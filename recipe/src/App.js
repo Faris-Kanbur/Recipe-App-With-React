735bf6f44485619efda20b435c282c5a6034300c
faris <faris38kanbur@gmail.com>
2020-12-27 23:51:57 +0100

    add fetch func to get info from API

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react'
 import './App.css';
+import axios from "axios";
 
 function App() {
+  
+  const options = {
+    method: 'GET',
+    url: 'https://edamam-recipe-search.p.rapidapi.com/search',
+    params: {q: 'chicken'},
+    headers: {
+      'x-rapidapi-key': '348c7742d6msh940b979d425b380p17a566jsn1ee6183a35ea',
+      'x-rapidapi-host': 'edamam-recipe-search.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+    
+    
+    useEffect(() => {
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <form className="search-form" >
+            <input className="search-bar" type="text" />
+            <button className="search-button" type="submit"> Search </button>
+        </form>
+    </div> 
   );
-}
+};
 
 export default App;
